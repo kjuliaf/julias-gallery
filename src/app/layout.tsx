0459c255a591c10dd91b22d1cfa68d5ca7ba8378
Julia Forsberg <103151478+kjuliaf@users.noreply.github.com>
2025-09-22 00:52:00 +0200

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import { Mulish, Parisienne } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
 				<Header />
 				<main>{children}</main>
 				<Footer />
+				<Analytics />
 			</body>
 			<PrismicPreview repositoryName={repositoryName} />
 		</html>
